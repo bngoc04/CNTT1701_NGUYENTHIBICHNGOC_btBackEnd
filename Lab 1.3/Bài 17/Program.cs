a4e1d1943edb4f8e3bafe21b1689b0a74e11a59f
Bich Ngoc <nguyenthibichngoc041005@gmail.com>
2025-04-08 14:41:24 +0700

    Add files

---
+++ Lab 1.3/Bài 17/Program.cs
@@ -0,0 +1,12 @@
+﻿// Bài 17: Mỗi một điểm trong mặt phẳng được xác đinh duy nhất bởi hai giá trị là hoành độ và tung độ.
+//      1.Hãy xây dựng lớp Diem cùng với chứa các đối tượng diểm trong mặt phẳng và xây dựng
+//      phương thức sau:
+//          -Toán tử tạo lập
+//          - Phương thức in một đối tượng thuộc lớp Diem
+//          - Tính khoảng cách giữa hai điểm
+//      2. Xây dựng lớp HinhTron chứa các đối tượng là các hình tròn với 2 thuộc tính là 1 đối tượng thuộc lớp Diem để xác định tâm của hình tròn một giá trị nguyên để xác định bán kính của hình tròn.
+//      Cài đặt các phương thức:
+//          -Xây dựng các toán tử tạo lập: HinhTron(),
+//          -HinhTron(Diem d, float bk)
+//          - Tính chu vi, diện tich hình tròn;
+//          -Nhập vào một danh sách các hình tròn, hiển thị thông tin về hình tròn giao với nhiều hình tròn khác nhất trong danh sách những hình tròn đã nhập vào.
